12251dd65a6e1eeac21858a3899fd4f55dfd0c25
Odunayo Akinlade <odunayoakinlade@Odunayos-MacBook-Air.local>
2023-04-20 21:19:56 +0100

    Add Share Feature

--- blog/models.py
+++ blog/models.py
@@ -2,38 +2,49 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.template.defaultfilters import slugify
 # Create your models here.
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super(PublishedManager,
-                     self).get_queryset()\
-                     .filter(status='published')
+                     self).get_queryset().filter(status='published')
 
 class Post (models.Model):
     STATUS_CHOICES = (
-    ('draft', 'Draft'),
-    ('published', 'Published'),
+        ('draft', 'Draft'),
+        ('published', 'Published'),
     )
-    title = models.CharField (max_length=250)
-    slug = models.SlugField(max_length=250),
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(User,
+                              on_delete=models.CASCADE,
+                              related_name='blog_posts')
     body = models.TextField()
-    publish = models.DateTimeField (default=timezone.now)
+    publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    status = models.CharField(max_length=10,
+                              choices=STATUS_CHOICES,
+                              default='draft')
+
     objects = models.Manager() #The default manager.
     published = PublishedManager() # Our custom manager.
-
-    def get_absolute_url(self):
-        return reverse('blog:post_detail',
-                       args=[self.publish.year,
-                             self.publish.month,
-                             self.ppublish.day, self.slug])
 
     class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
+    
+    def save (self):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save()
